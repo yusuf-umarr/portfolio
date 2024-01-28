3a641bd8a9f9c48245c6e96d41d97d64f3bca4f8
yusuf <yusufumar@yusufs-MacBook-Pro.local>
2024-01-28 11:06:17 +0100

    feat:implemeted section

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,11 +1,23 @@
+import { useRef } from "react"
 import "./parallax.scss"
+import { motion, useScroll } from 'framer-motion'
+
 const Parallax = ({type}) => {
+
+  const ref = useRef()
+
+  const {scrollYProgress} = useScroll({
+    target:ref
+  })
   return (
-    <div className="parallax">
-        <h1>{type ==="services"? "What We Do?":"What We Did?"}</h1>
-        <div className="mountains"></div>
-        <div className="platnet"></div>
-        <div className="stars"></div>
+    <div className="parallax" style={{background: type==="services"
+    ? "linear-gradient(180deg, #111132, #0c0c1d)"
+    : "linear-gradient(180deg,  #111132, #505064)",
+    }}>
+        <motion.h1>{type ==="services"? "What We Do?":"What We Did?"}</motion.h1>
+        <motion.div className="mountains"></motion.div>
+        <motion.div className="planets"></motion.div>
+        <motion.div className="stars"></motion.div>
     </div>
   )
 }
